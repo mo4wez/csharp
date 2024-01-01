b0f35532087cb49db805a333604210a2d5fcb1bc
moawezz <moawez.de@gmail.com>
2024-01-01 14:44:15 +0330

    OOP in C#

---
+++ OOP/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box myBox = new Box();
+            Box.Material = "Iron";
+            myBox.length = 3.25;
+            myBox.breadth = 4.3;
+            myBox.height = 8.0;
+            myBox.color = "Brown";
+
+            Console.WriteLine("length: {0}", myBox.length);
+            Console.WriteLine("breadth: {0}", myBox.breadth);
+            Console.WriteLine("heigth: {0}", myBox.height);
+            Console.WriteLine("color: {0}", myBox.color);
+            Console.WriteLine("Box Material: {0}", Box.Material);
+        }
+    }
+
+    class Box
+    {
+        public double length;
+        public double breadth;
+        public double height;
+        public string color;
+        public static string Material;
+    }
+}
